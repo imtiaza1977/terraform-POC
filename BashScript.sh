c741c2d4045ce5baf49bd0cb032a4ad5b658d115
imtiaza1977 <adeel.imtiaz@gmail.com>
2022-06-02 15:02:34 +0500

    Bash File Comments Updated

--- BashScript.sh
+++ BashScript.sh
@@ -2,6 +2,7 @@
 az login
 az account set --subscription "Terraform-Deployment" 
 
+#Set variables for Storage Account and Key Vault that support the Terraform Implementation
 RESOURCE_GROUP_NAME="awspre-infra"
 STORAGE_ACCOUNT_NAME="awspresto"
 CONTAINER_NAME="awsprestate"
@@ -15,14 +16,14 @@ az group create --name $RESOURCE_GROUP_NAME --location koreacentral
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
 
-# Get storage account key (Only used if SPN not available)
+# Get storage account key 
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
 
 
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
 
-
+# Show detail for the purpose of this code
 echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
 echo "container_name: $CONTAINER_NAME"
 echo "access_key: $ACCOUNT_KEY"
